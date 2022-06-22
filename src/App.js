209525413b21b5329cb04e719f658a0f86ea6c15
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2022-06-22 10:56:17 +0600

    NestedRoute

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Courses from "./Pages/Courses";
+import Search from "./Components/Search";
+import List from "./Components/List";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="courses">Courses</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />}>
+            <Route path="search" element={<Search />} />
+            <Route path="list" element={<List />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
